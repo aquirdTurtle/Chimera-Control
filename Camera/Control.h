bb1cba0dc1b77dcc871ee2e7d0b405795605e96e
Mark Brown <o.mark.brown@gmail.com>
2016-05-02 21:02:45 -0600

    Trying other method to get rid of those pesky files

--- Camera/Control.h
+++ Camera/Control.h
@@ -4,9 +4,14 @@
 
 struct Control
 {
+	// main window handle used by win32
 	HWND hwnd;
+	// positions for different camera modes
 	RECT kineticSeriesModePos;
 	RECT continuousSingleScansModePos;
 	RECT accumulateModePos;
+	// this is an extra displacement that gets added when internal trigger mode is on.
+	RECT internalTriggerAddition;
+	// a string that indicates the general font type. Exact size determined by this and size of system window.
 	std::string fontType;
 };
