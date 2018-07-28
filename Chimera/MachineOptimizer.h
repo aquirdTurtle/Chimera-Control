d542c3d6117f208cd6bd4dcaed99365c0de9d0d9
Mark Brown <o.mark.brown@gmail.com>
2018-07-28 12:18:15 +0200

    outlined machineOptimizer header file, just getting started

---
+++ Chimera/MachineOptimizer.h
@@ -0,0 +1,47 @@
+﻿#pragma once
+#include "Control.h"
+#include <array>
+
+/// just roughly right now...
+
+struct optimizationAlgorithm
+{
+	enum class which
+	{
+		HillCimbing,
+		NeuralNetwork,
+		Genetic,
+		RandomizedSearch
+	};
+	which fromStr ( std::string txt );
+	std::string toStr ( which alg );
+};
+
+
+struct optimizationSettings
+{
+	optimizationAlgorithm::which alg;
+	double tolerance;
+	double gain;
+	std::string config;
+};
+
+
+class MachineOptimizer
+{
+	public:
+		void initialize ( );
+		void handleOpenConfig ( );
+		void handleSaveConfig ( );
+		void handleNewConfig ( );
+		void runOptimization ( );
+	private:
+		// controls
+		Control<CStatic> header;
+		Control<CButton> optimizeButton;
+		// optimization settings
+		Control<CStatic> optimizationParametersHeader;
+		Control<CStatic> algorithmsHeader;
+		std::array<Control<CButton>, 4> algorithmRadios;
+
+};
