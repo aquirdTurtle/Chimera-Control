f1df6e02fca5429ac408a0371a20759282e900ab
Mark Brown <o.mark.brown@gmail.com>
2018-11-22 17:49:20 -0700

    Tolerance and gain now part fo servo listview, can set indv values

--- Chimera/Servo.h
+++ Chimera/Servo.h
@@ -5,11 +5,13 @@
 struct servoInfo
 {
 	std::string servoName="";
-	bool active=false;
+	bool active = false;
 	UINT aiInputChannel=0;
 	UINT aoControlChannel=0;
 	double setPoint=0;
 	double controlValue=0;
+	double tolerance = 0.02;
+	double gain = 0.02;
 	bool servoed=false;
 	std::vector<std::pair<DioRows::which, UINT> > ttlConfig;
 };
