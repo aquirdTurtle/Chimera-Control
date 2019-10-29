57b6b601ecbc94771549b1235f90b952385c9200
maxKOL00 <mako2910@colorado.edu>
2019-10-29 11:42:34 -0600

    sad

---
+++ Chimera/DmCore.cpp
@@ -0,0 +1,69 @@
+#include "stdafx.h"
+#include "DmCore.h"
+#include "Expression.h"
+#include <iostream>
+
+DmCore::DmCore(std::string Number, bool safemodeOption) : DM(safemodeOption){
+	serial = Number;
+	boolOfSerial = true;
+	
+	valueArray = std::vector<double>(DM.getActuatorCount(), 0.0);
+}
+
+//DmCore::DmCore(bool safemodeOption) : DM(safemodeOption) {
+//	serial = "";
+//	boolOfSerial = false;
+//}
+
+void DmCore::setSerial(std::string Number) {
+	serial = Number;
+	boolOfSerial = true;
+}
+
+std::string DmCore::getDeviceInfo() {
+	if (!boolOfSerial) {
+		thrower("No Device");
+	}
+	else {
+		return DM.getDeviceInfo(serial);
+	}
+}
+
+void DmCore::initialize() {
+	if (!boolOfSerial) {
+		thrower("serial number not set for DM");
+	}
+	else {
+		DM.open(serial);
+	}
+}
+
+void DmCore::setMap() {
+	DM.LoadMap(map_lut.data());
+}
+
+void DmCore::pokePiston(unsigned int piston, double value) {
+	DM.setSingle(piston, value);
+}
+void DmCore::zeroPistons() {
+	DM.reset();
+}
+
+void DmCore::readDMArray() {
+	std::vector<double> testArray = std::vector<double>(DM.getActuatorCount(), 0.0);
+
+	DM.getArray(testArray.data(), DM.getActuatorCount());
+}
+
+void DmCore::testMirror() {
+	for (auto& val : valueArray)
+	{
+		val = 0.5;
+	}
+	//initialize();
+	setMap();
+	std::vector<double> testArray = std::vector<double>(DM.getActuatorCount(), 0.0);
+	DM.setArray(valueArray.data(), map_lut.data());
+	readDMArray();
+}
+
---
+++ Chimera/DmCore.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "DmFlume.h"
+#include "Expression.h"
+
+class DmCore {
+	public:
+		DmCore::DmCore(std::string Number, bool safemodeOption);
+		std::string getDeviceInfo();
+		void setSerial(std::string Number);
+		void initialize();
+		void setMap();
+		void pokePiston(unsigned int piston, double value);
+		void zeroPistons();
+		void readDMArray();
+		void testMirror();
+	private:
+		DmFlume DM;
+		std::vector<unsigned int> map_lut = std::vector<unsigned int>(MAX_DM_SIZE, 0);
+		std::vector<double> valueArray;
+		std::string serial;
+		bool boolOfSerial;
+};
