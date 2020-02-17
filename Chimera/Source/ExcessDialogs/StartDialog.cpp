f3114ccf52181bd489a33b68a356435def0289af
Mark Brown <o.mark.brown@gmail.com>
2020-02-17 00:46:22 -0700

    some minor changes to some dialogs to make them look better / more theme consistent.

--- Chimera/Source/ExcessDialogs/StartDialog.cpp
+++ Chimera/Source/ExcessDialogs/StartDialog.cpp
@@ -12,6 +12,7 @@ StartDialog::StartDialog( std::string msg, UINT id ) : CDialog::CDialog( id )
 IMPLEMENT_DYNAMIC( StartDialog, CDialog )
 
 BEGIN_MESSAGE_MAP( StartDialog, CDialog )
+	ON_WM_CTLCOLOR ()
 	ON_WM_SIZE( )
 	ON_COMMAND( IDOK, &StartDialog::OnOK )
 	ON_COMMAND( IDCANCEL, &StartDialog::OnCancel )
@@ -24,27 +25,25 @@ void StartDialog::OnSize(UINT s, int width, int height )
 	cancelBtn.rearrange( width, height );
 }
 
-
 void StartDialog::OnOK()
 {
 	EndDialog( TRUE );
 }
-
 
 void StartDialog::OnCancel( )
 {
 	EndDialog( FALSE );
 }
 
-
 BOOL StartDialog::OnInitDialog( )
 {
 	RECT r;
 	GetClientRect( &r );
 	edit.sPos = { 0, 0, r.right, r.bottom-30};
-	edit.Create( WS_CHILD | WS_VISIBLE | ES_MULTILINE | ES_AUTOVSCROLL | WS_VSCROLL
-				 | ES_READONLY, edit.sPos, this, NULL );
+	edit.Create( WS_CHILD | WS_VISIBLE | ES_MULTILINE | ES_AUTOVSCROLL | WS_VSCROLL	| ES_READONLY, edit.sPos, this, NULL );
 	edit.SetWindowTextA( startMsg.c_str() );
+	edit.SetBackgroundColor (false, _myRGBs["Main-Bkgd"]);
+
 	okBtn.sPos = { 0, r.bottom - 30, r.right/2, r.bottom };
 	okBtn.Create( "OK", NORM_PUSH_OPTIONS, okBtn.sPos, this, IDOK );
 	cancelBtn.sPos = { r.right/2, r.bottom - 30,  r.right, r.bottom };
@@ -54,16 +53,16 @@ BOOL StartDialog::OnInitDialog( )
 	DWORD start = 0, end = 0;
 	std::size_t prev, pos;
 	std::string word, analysisWord;
-	CHARFORMAT syntaxFormat;
-	memset( &syntaxFormat, 0, sizeof( CHARFORMAT ) );
+	CHARFORMAT syntaxFormat = { 0 };
 	syntaxFormat.cbSize = sizeof( CHARFORMAT );
 	syntaxFormat.dwMask = CFM_COLOR;
 	while ( std::getline( streamObj, line ) )
 	{
+		syntaxFormat.crTextColor = _myRGBs["Text"];
 		DWORD lineStartCoordingate = start;
-
+		edit.SetSel (lineStartCoordingate, std::string::npos);
+		edit.SetSelectionCharFormat (syntaxFormat);
 		prev = 0;
-
 		while ( (pos = line.find_first_of( " \t\r\n", prev )) != std::string::npos )
 		{
 			end = lineStartCoordingate + pos;
@@ -108,3 +107,9 @@ BOOL StartDialog::OnInitDialog( )
 	return TRUE;
 }
 
+HBRUSH StartDialog::OnCtlColor (CDC* pDC, CWnd* pWnd, UINT nCtlColor)
+{
+	pDC->SetTextColor (_myRGBs["Text"]);
+	pDC->SetBkColor (_myRGBs["Main-Bkgd"]);
+	return *_myBrushes["Main-Bkgd"];
+}
