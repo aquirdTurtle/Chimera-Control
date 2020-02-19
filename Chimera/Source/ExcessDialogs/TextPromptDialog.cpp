b40fd693d08d7899c3ad12081b71615965752e9c
Mark Brown <o.mark.brown@gmail.com>
2020-02-18 21:10:41 -0700

    improved textpromptdialog style and added default string

--- Chimera/Source/ExcessDialogs/TextPromptDialog.cpp
+++ Chimera/Source/ExcessDialogs/TextPromptDialog.cpp
@@ -13,21 +13,44 @@ BEGIN_MESSAGE_MAP(TextPromptDialog, CDialog)
 	ON_COMMAND(ID_ACCELERATOR_F5 , &TextPromptDialog::catchf5)
 END_MESSAGE_MAP()
 
-void TextPromptDialog::catchf5( )
+void TextPromptDialog::catchf5( ) { }
+
+HBRUSH TextPromptDialog::OnCtlColor (CDC* pDC, CWnd* pWnd, UINT nCtlColor)
 {
-	errBox( "hi!" );
+	switch (nCtlColor)
+	{
+		case CTLCOLOR_EDIT:
+		{
+			pDC->SetTextColor (_myRGBs["MainWin-Text"]);
+			pDC->SetBkColor (_myRGBs["Interactable-Bkgd"]);
+			return *_myBrushes["Interactable-Bkgd"];
+		}
+		default:
+		{
+			pDC->SetTextColor (_myRGBs["Text"]);
+			pDC->SetBkColor (_myRGBs["Main-Bkgd"]);
+			return *_myBrushes["Main-Bkgd"];
+		}
+	}
 }
 
 
 BOOL TextPromptDialog::OnInitDialog()
 {
-	DWORD options = WS_CHILD | WS_VISIBLE | WS_BORDER;
+	DWORD options = WS_CHILD | WS_VISIBLE | WS_BORDER | ES_CENTER;
 	if (passwordOption)
 	{
 		options |= ES_PASSWORD;
 	}
-	description.Create(cstr(descriptionText), options | ES_READONLY, { 0,0,1000,75 }, this, 0);
-	prompt.Create( options, { 0,75,1000,150 }, this, 0 );
+	RECT rect;
+	GetClientRect (&rect);
+	description.Create(cstr(descriptionText), options | ES_READONLY, { 0,0,rect.right,75 }, this, 0);
+	prompt.Create( options , { 0,75,rect.right,rect.bottom - 50 }, this, 0 );
+	prompt.SetWindowText (initStringValue.c_str());
+	okBtn.sPos = { 0, rect.bottom - 50, rect.right/2, rect.bottom };
+	okBtn.Create ("OK", NORM_PUSH_OPTIONS, okBtn.sPos, this, IDOK);
+	cancelBtn.sPos = { rect.right/2, rect.bottom - 50, rect.right, rect.bottom };
+	cancelBtn.Create ("CANCEL", NORM_PUSH_OPTIONS, cancelBtn.sPos, this, IDCANCEL);
 	return TRUE;
 }
 
