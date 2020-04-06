170e3a4265262ab4a3c957cf27b8d80378b95431
Mark Brown <o.mark.brown@gmail.com>
2020-04-05 20:48:48 -0600

    Properly ported Notesystem to Qt! Still some issues with redraws covering other controls, but basically working, which is very exciting.

--- Chimera/Source/ConfigurationSystems/NoteSystem.h
+++ Chimera/Source/ConfigurationSystems/NoteSystem.h
@@ -5,17 +5,23 @@
 #include "ConfigurationSystems/Version.h"
 #include "ConfigurationSystems/ConfigStream.h"
 #include "GeneralObjects/commonTypes.h"
+#include "QWinWidget.h"
+#include <QLabel>
+#include <QTextEdit>
 
+ 
 class NoteSystem
 {
 	public:
 		void handleSaveConfig(ConfigStream& saveFile);
 		void handleOpenConfig(ConfigStream& openFile );
 		void setConfigurationNotes(std::string notes);
-		void initialize( POINT& topLeftPos, CWnd* parentWindow, int& id, cToolTips& tooltips );		
+		void initialize( POINT& topLeftPos, QWinWidget* parent, int& id, cToolTips& tooltips );
 		std::string getConfigurationNotes();
 		void rearrange(int width, int height, fontMap fonts);
 	private:
-		Control<CEdit> configNotes;
-		Control<CStatic> configNotesHeader;
+		//Control<CEdit> configNotes;
+		//Control<CStatic> configNotesHeader;
+		QLabel* header;
+		QTextEdit* edit;
 };
